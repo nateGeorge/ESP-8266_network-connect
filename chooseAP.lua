f3c557244b7ff04d9601ef20690fa3c05f62bd97
wordsforthewise <wordsforthewise@gmail.com>
2015-08-16 07:55:15 -0500

    moved form html into another file

--- chooseAP.lua
+++ chooseAP.lua
@@ -1,11 +1,12 @@
 -- had to chunk up sending of webpage, to deal with low amounts of memory on ESP-8266 devices
 -- surely a more elegant way to do it
+
+dofile("makeChooseAPHTML.lua")
 local SSID = nil
 local pass = nil
 local otherSSID = nil
 local errMsg = nil
 local savedNetwork = false
-local SSIDs = {}
 local resetTimer = 15 -- for resetting the module after successfully connecting to chosen network
 -- lookup table for wifi.sta.status()
 local statusTable = {}
@@ -19,23 +20,12 @@ wifi.sta.disconnect()
 wifi.setmode(wifi.STATIONAP)
 print('wifi status: '..wifi.sta.status())
 
--- opens saved list of nearby networks and puts into SSIDs table
-file.open('networkList','r')
-local counter = 0
-local line = ""
-while true do
-    line = file.readline()
-    if line == nil then break end
-    counter = counter + 1
-    SSIDs[counter] = line
-end
-
 local cfg = {}
 cfg.ssid = "mysticalNetwork"
 cfg.pwd = "mystical5000"
 wifi.ap.config(cfg)
 local srv=net.createServer(net.TCP, 300)
-print('connect to \''..cfg.ssid..'\', password \''..cfg.pwd..'\', ip '..wifi.ap.getip())
+print("connect to \'"..cfg.ssid.."\", password \""..cfg.pwd.."\", ip "..wifi.ap.getip())
 cfg = nil
 srv:listen(80,function(conn)
 conn:on("receive", function(client,request)
@@ -71,7 +61,7 @@ conn:on("receive", function(client,request)
             pass = "aaaaaaaa"
         end
         print(SSID..', '..pass)
-        -- TO-DO: add timout for connection attempt
+        -- TO-DO: add timeout for connection attempt
         local connectStatus = wifi.sta.status()
         print(connectStatus)
         sendHeader(client)
@@ -133,44 +123,20 @@ conn:on("receive", function(client,request)
 end)
 end)
 
-function sendHeader(client)
-    -- write header to client
-    buf = ""
-    buf = buf.."<!DOCTYPE html><html><head><style>h2{font-size:500%; font-family:helvetica} "
-    buf = buf.."p{font-size:200%; font-family:helvetica}</style>"
-    buf = buf.."</head><div style = \"width:80%; margin: 0 auto\">"
-    client:send(buf)
-end
-
-function sendForm(client, errMsg)
-    buf = ""
-    -- send top of form to client
-    buf = buf.."<center><h1>Choose a network to join:</h1></center>"
-    buf = buf.."<form align=\"left\" method=\"POST\" autocomplete=\"off\">"
-    buf = buf.."<p><u><b>1. Choose network:</u></b><br>"
-    client:send(buf)
-    buf = ""
-    -- send network names one at a time; if there are lots of networks the ESP can run out of memory
-    for i,network in pairs(SSIDs) do
-        netSubSpaces, _ = string.gsub(network, " ", "%%20")
-        buf = "<input type=\"radio\" name=\"SSID\" value=\""..netSubSpaces.."\">"..network.."<br>"
-        client:send(buf)
-        buf = ""
+function sendWebpage(client, pageFile)
+    file.open(pageFile)
+    line = file.readline()
+    line = string.sub(line,1,string.len(line)-1) --hack to remove CR/LF
+    while (line~=nul) do
+        client:send(line)
+        if line == "<div style = \"width:80%; margin: 0 auto\">" do
+            -- add warning about password<8 characters if needed
+            if (errMsg~=nil) then
+                buf = buf.."<br><br>"..errMsg
+                client:send(buf)
+            end
+        end
+        line = file.readline()
     end
-    buf = buf.."other: <input type=\"text\" name=\"otherSSID\"><br><br>"
-    buf = buf.."<u><b>2. Enter password (or blank for none):</u></b><br><input type=\"text\" name=\"password\"><br><br>"
-    buf = buf.."<input style=\"font-size:30pt\" type=\"submit\" value=\"Submit\">"
-    buf = buf.."</p></form>"
-    client:send(buf)
-    buf = ""
-    -- add warning about password<8 characters if needed
-    if (errMsg~=nil) then
-        buf = buf.."<br><br>"..errMsg
-        client:send(buf)
-    end
-    buf = buf.."<br><br><br><form align=\"center\" method=\"POST\">"
-    buf = buf.."<input type=\"hidden\" name=\"deleteSaved\" value=\"true\">"
-    buf = buf.."<input type=\"submit\" value=\"Delete all saved networks\" style=\"font-size:30pt; color:red\"></form></html></div>"
-    client:send(buf)
-    buf = ""
+    file.close()
 end
