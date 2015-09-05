f84f365ffefaddafbe1bdebdedc7b3b1caecd24f
wordsforthewise <wordsforthewise@gmail.com>
2015-09-05 08:25:29 -0600

    updated for water management project

--- init.lua
+++ init.lua
@@ -1,15 +1,15 @@
 dataToSend = {}
 wifi.setmode(1)
 
-dofile('getAPlist.lua')
-
 tmr.alarm(1,5000,1,function ()
     local wifiStat = wifi.sta.status()
     print(wifiStat)
     if (wifiStat == 5) then
         tmr.stop(1)
+        dofile('logdata.lua')
     end
     if (wifiStat ~= 5 and wifiStat ~= 1) then
+        dofile('getAPlist.lua')
         if (file.open('networks')) then
             file.close('networks')
         end
